efd918d04d73ebc2d7862e98096b87d8d8d6882c
regha <rega.blank@gmail.com>
2018-10-02 21:39:55 +0700

    [rega] edit calculator

--- src/Main.kt
+++ src/Main.kt
@@ -3,12 +3,12 @@
 * */
 
 fun calculator(angka1:Float,angka2:Float){
-    println ("$angka1 + $angka2 = "+ (angka1+angka1))
-    println("$angka1 - $angka2 = "+ (angka1-angka1))
-    println ("$angka1 : $angka2 = "+angka1/angka1)
-    println ("$angka1 x $angka2 = "+angka1*angka1)
+    println ("$angka1 + $angka2 = "+ (angka1+angka2))
+    println("$angka1 - $angka2 = "+ (angka1-angka2))
+    println ("$angka1 : $angka2 = "+ (angka1/angka2))
+    println ("$angka1 x $angka2 = "+ (angka1*angka2))
 }
 fun main(args: Array<String>){
-    calculator(1.0f,2.0f)
+    calculator(2.0f,4.0f)
 }
 
