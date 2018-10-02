439e8815dbe032bdaae9b44919beef64f7681260
regha <rega.blank@gmail.com>
2018-10-02 21:35:39 +0700

    [rega] init calculator

---
+++ src/Main.kt
@@ -0,0 +1,14 @@
+/*
+*
+* */
+
+fun calculator(angka1:Float,angka2:Float){
+    println ("$angka1 + $angka2 = "+ (angka1+angka1))
+    println("$angka1 - $angka2 = "+ (angka1-angka1))
+    println ("$angka1 : $angka2 = "+angka1/angka1)
+    println ("$angka1 x $angka2 = "+angka1*angka1)
+}
+fun main(args: Array<String>){
+    calculator(1.0f,2.0f)
+}
+
