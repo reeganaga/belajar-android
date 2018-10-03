752441fcebea203f894bff603f4136bbfe835c79
regha <rega.blank@gmail.com>
2018-10-03 20:49:23 +0700

    [rega] submit tugas sesi 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,67 +1,19 @@
-/*
-*
-* */
+fun check_array(nilai : Array<Any>) {
+    for (item in nilai) {
+        when(item){
+            is Int -> println("[$item] is integer")
+            is Boolean -> println("[$item] is Boolean")
+            is Float -> println("[$item] is Float")
+            is Double -> println("[$item] is Double")
+            is String -> println("[$item] is String")
+            is Char -> println("[$item] is Char")
+        }
 
-//fun calculator(angka1:Float,angka2:Float){
-//    println ("$angka1 + $angka2 = "+ (angka1+angka2))
-//    println("$angka1 - $angka2 = "+ (angka1-angka2))
-//    println ("$angka1 : $angka2 = "+ (angka1/angka2))
-//    println ("$angka1 x $angka2 = "+ (angka1*angka2))
-//}
-//fun main(args: Array<String>){
-//    calculator(2.0f,4.0f)
-//
-//}
+    }
+}
 
 fun main(args: Array<String>) {
-    var a: String? = null
-    if (a.isNullOrEmpty()) {
-        println(a)
-    }
-    var b = if (null == a) "bilangan null" else "bukan null"
-    println(b)
-
-
-    var c = 8
-    when {
-        c % 3 == 0 -> print("habis dibagi 3")
-        else -> {
-            println("tidak habis dibagi 3")
-        }
-    }
-
-    //for
-    var d = 10
-    for (i in 0..d) {
-        println(i)
-    }
-    println("----")
-    for (i in 0 until d) {
-        println(i)
-    }
-    println("----")
-    for (i in d downTo 0) {
-        println(i)
-    }
-
-    val test_array = arrayOf(1, 2, 3, 4, "test", false)
-    for (i in test_array) { // i : Int ==> memaksakan nilai yang di array bernilai integer
-        println(i)
-    }
-
-//    while
-    var e = 10
-    while (e < 100) {
-        println(e);
-        e += 10;
-    }
-    e=0
-    do {
-        println(e)
-        e++
-    }while (e<20)
-
-
-
+    var test_array = arrayOf(1, 2, "tiga", false, 'T', 6.5)
+    check_array(test_array)
 }
 
